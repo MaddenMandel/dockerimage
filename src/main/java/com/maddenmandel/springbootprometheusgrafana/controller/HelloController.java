75bb1918f842c25fdc8a9a3af09bfc423536f6c0
zhangxuegang <zhangxuegang@delianyikong.cn>
2024-10-25 17:09:29 +0800

    Add springboot-prometheus-grafana

---
+++ src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.maddenmandel.springboot.prometheus.grafana.controller;
+
+import io.prometheus.client.spring.web.PrometheusTimeMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    @PrometheusTimeMethod(name = "hello_controller_say_hello_duration_seconds", help = "Some helpful info here")
+    public String sayHello() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/maddenmandel/springbootprometheusgrafana/DockerImageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maddenmandel.springboot.prometheus.grafana;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockerImageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
