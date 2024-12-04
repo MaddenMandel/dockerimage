3354e0159099bf2d6a755d1e308f9d439aac98c5
zhangxuegang <zhangxuegang@delianyikong.cn>
2024-12-04 11:23:41 +0800

    Add log

--- src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
+++ src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
@@ -1,15 +1,20 @@
 package com.maddenmandel.springbootprometheusgrafana.controller;
 
 import io.micrometer.core.annotation.Timed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
 
     @GetMapping("/hello")
     @Timed(value = "hello_controller_say_hello_duration_seconds", description = "Time taken to return hello")
     public String sayHello() {
+        logger.info("This is a test log message");
         return "hello";
     }
 }
