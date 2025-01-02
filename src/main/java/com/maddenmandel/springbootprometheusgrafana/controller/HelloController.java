63431e70300b0b10d6a5f5d9eabbd4eba2505372
maddenrichard <zhangxuegang@gmail.com>
2025-01-02 21:33:08 +0800

    Fix the issue where metrics cannot be scraped by Prometheus.

--- src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
+++ src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.maddenmandel.springbootprometheusgrafana.controller;
 
 import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
-
     @GetMapping("/hello")
     @Timed(value = "hello_controller_say_hello_duration_seconds", description = "Time taken to return hello")
     public String sayHello() {
+        log.info("Saying hello");
         return "hello";
     }
 }
