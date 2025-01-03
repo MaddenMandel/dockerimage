33bbd8c4ba9d90be084c3789d5fb4c77c7258a56
maddenrichard <zhangxuegang@gmail.com>
2025-01-03 23:20:31 +0800

    Add Jaeger.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
 FROM openjdk:17-jdk-slim
 VOLUME /tmp
-COPY target/springboot-prometheus-grafana-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+
+COPY target/springboot-prometheus-grafana-0.0.1-SNAPSHOT.jar /app/app.jar
+COPY env/opentelemetry-javaagent.jar /app/opentelemetry-javaagent.jar
+COPY env/otel-collector-config.yaml /app/otel-config.yaml
+
+WORKDIR /app
+
+ENTRYPOINT ["java", "-javaagent:/app/opentelemetry-javaagent.jar", \
+            "-Dotel.exporter=otlp", \
+            "-Dotel.exporter.otlp.endpoint=http://opentelemetry-collector:55680", \
+            "-Dotel.service.name=springboot-prometheus-grafana", \
+            "-Dotel.debug=true", \
+            "-jar", "app.jar"]
--- src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
+++ src/main/java/com/maddenmandel/springbootprometheusgrafana/controller/HelloController.java
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+	@GetMapping("/hello")
+	@Timed(value = "hello_controller_say_hello_duration_seconds", description = "Time taken to return hello")
+	public String sayHello() {
+		logger.info("This is a test log message");
+		log.info("Saying hello");
+		return "hello";
+	}
 
-    @GetMapping("/hello")
-    @Timed(value = "hello_controller_say_hello_duration_seconds", description = "Time taken to return hello")
-    public String sayHello() {
-        logger.info("This is a test log message");
-        log.info("Saying hello");
-        return "hello";
-    }
 }
